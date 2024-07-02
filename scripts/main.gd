9cea0ba57aeddb5a60155a02105c71b2cf3b6927
NIK-98 <schrot-schrot@web.de>
2024-07-02 18:08:37 +0200

    android steuerkreutz optimiert

--- scripts/main.gd
+++ scripts/main.gd
@@ -158,3 +158,43 @@ func _on_tap_pressed():
 
 func _on_tap_released():
 	Input.action_release("Info")
+
+
+func _on_up_left_pressed():
+	Input.action_press("up")
+	Input.action_press("left")
+
+
+func _on_up_left_released():
+	Input.action_release("up")
+	Input.action_release("left")
+
+
+func _on_up_right_pressed():
+	Input.action_press("up")
+	Input.action_press("right")
+
+
+func _on_up_right_released():
+	Input.action_release("up")
+	Input.action_release("right")
+
+
+func _on_down_left_pressed():
+	Input.action_press("down")
+	Input.action_press("left")
+
+
+func _on_down_left_released():
+	Input.action_release("down")
+	Input.action_release("left")
+
+
+func _on_down_right_pressed():
+	Input.action_press("down")
+	Input.action_press("right")
+	
+	
+func _on_down_right_released():
+	Input.action_release("down")
+	Input.action_release("right")
