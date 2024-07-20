70959705b34b2b72fda2e510e3568ce1bcf63c4a
NIK-98 <schrot-schrot@web.de>
2024-07-20 17:18:31 +0200

    debug errors minimiert

--- scripts/bombe.gd
+++ scripts/bombe.gd
@@ -6,19 +6,20 @@ extends Node2D
 const  bomb_radius = 4
 	
 
-@rpc("any_peer","call_local")
+@rpc("any_peer","call_local", "unreliable")
 func aktivate_bombe(cell: int):
 	var tile_position = map.local_to_map(position)
 	for x in range(-bomb_radius,bomb_radius):
 		for y in range(-bomb_radius,bomb_radius):
 			var pos = Vector2i(x, y) + tile_position
 			map.set_cell(0, pos, cell, Vector2i(0,0))
+	if is_multiplayer_authority():
+		queue_free()
 	
 
 
 func _on_area_2d_area_entered(area):
-	if not is_multiplayer_authority() or not multiplayer.has_multiplayer_peer():
-		return
 	if area.get_parent().is_in_group("player"):
 		aktivate_bombe.rpc(area.get_parent().color_cell)
-	queue_free()
+		
+		
