accc901ad7d5a5a053e59b30a6b965c28f560285
NIK-98 <schrot-schrot@web.de>
2024-07-26 18:05:42 +0200

    bomben farben bug behoben

--- scripts/bombe.gd
+++ scripts/bombe.gd
@@ -22,7 +22,8 @@ func aktivate_bombe(cell: int):
 	for x in range(-bomb_radius,bomb_radius):
 		for y in range(-bomb_radius,bomb_radius):
 			var pos = Vector2i(x, y) + tile_position
-			map.set_cell(0, pos, cell, Vector2i(0,0))
+			if map.get_cell_source_id(0,pos) != -1:
+				map.set_cell(0, pos, cell, Vector2i(0,0))
 	cleaners_count += 1
 	clean = true
 	
