7359e9add9e9586a813ca823591c499eefcccc34
NIK-98 <schrot-schrot@web.de>
2024-09-17 16:13:56 +0200

    Bugfixes
    -performance problemme minimiert
    -ip anzeige optimiert

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -7,11 +7,14 @@ var Max_clients = 6
 @onready var connectport = $Panel/CenterContainer/Net/Options/Option2/o4/port.text
 @onready var ip = $Panel/CenterContainer/Net/Options/Option2/o3/remote1/Remote.text
 @onready var ip_list = $Panel/CenterContainer/Net/Options/Option1/ScrollContainer/yourip/ip_list
+@onready var update_time_ips = $Panel/CenterContainer/Net/Options/Option1/ScrollContainer/yourip/Label2
+@onready var ips_update_timer = $ips_update_timer
 
 
 var loaded = false
 var esc_is_pressing = false
 var old_ips = []
+var local_address: PackedStringArray
 
 
 func save():
@@ -30,10 +33,11 @@ func save():
 	
 func _ready():
 	name = "UI"
+	get_local_ips()
 	get_tree().paused = true
 	
 	
-func _process(_delta):
+func _process(delta):
 	if not loaded:
 		loaded = true
 		name = "UI"
@@ -50,22 +54,20 @@ func _process(_delta):
 		get_parent().get_parent().get_node("CanvasLayer/Beenden").visible = true
 		get_parent().get_parent().get_node("CanvasLayer/Beenden/PanelContainer/VBoxContainer/Ja").grab_focus()
 	
-	
-	get_local_ips()
+	update_time_ips.text = str("update in ",floor(ips_update_timer.time_left),"s")
 
 
 func get_local_ips():
-	var local_interfaces = IP.get_local_interfaces()
-	for r in ip_list.get_children():
-		r.queue_free()
-	for i in local_interfaces:
-		if (OS.has_feature("windows") and i["friendly"].begins_with("Ethernet") or i["friendly"].begins_with("WLAN")) or ((OS.has_feature("linux") or OS.has_feature("android")) and i["name"].begins_with("en") or i["name"].begins_with("eth") or i["name"].begins_with("wl")):
-			for address in i["addresses"]:
-				if (address.split('.').size() == 4) and address.begins_with("10.") or check_address_bereich(address,16,31) or address.begins_with("192.168."):
-					var addr = preload("res://sceens/myip.tscn")
-					var new_addr = addr.instantiate()
-					new_addr.text = str(address)
-					ip_list.add_child(new_addr)
+	if IP.get_local_addresses() != local_address:
+		local_address = IP.get_local_addresses()
+		for r in ip_list.get_children():
+			r.queue_free()
+		for i in local_address:
+			if (i.split('.').size() == 4) and i.begins_with("10.") or check_address_bereich(i,16,31) or i.begins_with("192.168."):
+				var addr = preload("res://sceens/myip.tscn")
+				var new_addr = addr.instantiate()
+				new_addr.text = str(i)
+				ip_list.add_child(new_addr)
 		
 	
 	
@@ -163,3 +165,7 @@ func change_level(scene: PackedScene):
 		c.queue_free()
 	# Add new level.
 	level.add_child(scene.instantiate())
+
+
+func _on_ips_update_timeout():
+	get_local_ips()
