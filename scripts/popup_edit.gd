62652abb8f5472f9f01769b02d085230d67466d9
NIK-98 <schrot-schrot@web.de>
2025-01-22 15:57:15 +0100

    feher behebung bei eingabe im hauptmenü

--- scripts/popup_edit.gd
+++ scripts/popup_edit.gd
@@ -14,19 +14,20 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if DisplayServer.has_feature(DisplayServer.FEATURE_VIRTUAL_KEYBOARD):
-		if selected and not visible and DisplayServer.virtual_keyboard_get_height() != 0:
+	if OS.get_name() == "Android" or OS.get_name() == "IOS":
+		if selected and not visible:
+			await get_tree().create_timer(0.8).timeout
+			visible = true
 			grab_focus()
 			max_length = parent_fild_length
-			await get_tree().create_timer(0.7).timeout
-			visible = true
-			position.y = get_viewport_rect().size.y - DisplayServer.virtual_keyboard_get_height()
+			position.y = get_viewport_rect().size.y - DisplayServer.get_display_safe_area().size.y*2
 			position.x = (get_viewport_rect().size.x-size.x)/2
 	if ui.get_parent().get_parent().get_parent().has_node("Level/level/loby") and not ui.get_parent().get_parent().get_parent().get_node("Level/level/loby").visible:
 		set_process(false)
 
 
 func _on_button_pressed() -> void:
+	text = ""
 	position.y = old_hight
 	visible = false
 	ui.get_node("Panel/CenterContainer/Net/Options/Host_Connect").grab_focus()
@@ -38,3 +39,7 @@ func _on_button_pressed() -> void:
 func _on_text_changed(new_text: String) -> void:
 	if selected_node != null:
 		selected_node.text = new_text
+
+
+func _on_focus_exited() -> void:
+	_on_button_pressed()
